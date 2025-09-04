16590d9e24e0d2daeeccb5d2075854c8f39d4d86
murodjon29 <smurodjon983@gmail.com>
2025-09-04 13:07:06 +0500

    updated

--- src/api/brand/brand.controller.ts
+++ src/api/brand/brand.controller.ts
@@ -2,15 +2,18 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Upl
 import { BrandService } from './brand.service';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('brand')
 export class BrandController {
   constructor(private readonly brandService: BrandService) {}
 
   @Post()
-  @UseInterceptors(FilesInterceptor('file'))
-  create(@Body() createBrandDto: CreateBrandDto, @UploadedFile() file: Express.Multer.File | any) {
+  @UseInterceptors(FileInterceptor('file'))
+  create(
+    @Body() createBrandDto: CreateBrandDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     return this.brandService.create(createBrandDto, file);
   }
 
@@ -25,12 +28,15 @@ export class BrandController {
   }
 
   @Patch(':id')
-  @UseInterceptors(FilesInterceptor('file'))
-  update(@Param('id') id: string, @Body() updateBrandDto: UpdateBrandDto, @UploadedFile() file: Express.Multer.File | any) {
+  @UseInterceptors(FileInterceptor('file'))
+  update(
+    @Param('id') id: string,
+    @Body() updateBrandDto: UpdateBrandDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     return this.brandService.update(+id, updateBrandDto, file);
   }
 
-  
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.brandService.remove(+id);
